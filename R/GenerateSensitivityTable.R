f5f3bab862a5ca6e8d4ca2d8ce17b7f819ccebb2
Joseph Richey <jr247694@maverik.com>
2023-12-29 16:43:40 -0700

    Added documentation and updated description.

--- R/GenerateSensitivityTable.R
+++ R/GenerateSensitivityTable.R
@@ -16,7 +16,7 @@ GenerateSensitivityVector <- function(base, numSteps, stepSize) {
 }
 
 # Private helper function
-GenerateSensitivityValidateInputs <- function(table_formula,
+GenerateSensitivityValidateInputs <- function(tableFormula,
                                               var1Base,
                                               var2Base,
                                               var1NumSteps,
@@ -24,22 +24,22 @@ GenerateSensitivityValidateInputs <- function(table_formula,
                                               var1StepSize,
                                               var2StepSize,
                                               nDigits,
-                                              value_format,
+                                              valueFormat,
                                               returnTable) {
   # Validate inputs
   ## Check that formula is passed
-  if (missing(table_formula)) {
+  if (missing(tableFormula)) {
     stop("Please provide a valid formula.")
   }
 
   ## Check that formula has only two inputs.
-  arguments <- names(formals(table_formula))
+  arguments <- names(formals(tableFormula))
   if (length(arguments) != 2) {
     stop("The formula must have exactly two variables.")
   }
 
   ## Check that the formula evalutes to a number. If it doesn't, throw a warning.
-  if(is.na(table_formula(var1Base, var2Base))) {
+  if(is.na(tableFormula(var1Base, var2Base))) {
     warning(paste("The formula you provided results in a NA value with ",
                   var1Base, "and", var2Base, "as inputs. You may want to check
                   your base inputs or formula."))
@@ -48,7 +48,7 @@ GenerateSensitivityValidateInputs <- function(table_formula,
   ## If the formula results in an error, throw an error.
   tryCatch(
     {
-      table_formula(var1Base, var2Base)
+      tableFormula(var1Base, var2Base)
     },
     error = function(e) {
       stop(paste("The formula you provided does not evaluate with", var1Base,
@@ -80,13 +80,40 @@ GenerateSensitivityValidateInputs <- function(table_formula,
     stop("Please ensure you are passing a logical value for returnTable.")
   }
 
-  if(!(value_format %in% c('number', 'percent'))) {
-    stop(paste0("Your input of ", value_format, " is not number or percent. Please enter a valid argument."))
+  if(!(valueFormat %in% c('number', 'percent'))) {
+    stop(paste0("Your input of ", valueFormat, " is not number or percent. Please enter a valid argument."))
   }
 }
 
-
-GenerateSensitivityTable <- function(table_formula,
+#' Generate a sensitivity table given a two input formula.
+#'
+#' @param tableFormula A formula with two inputs that will define the sensitivity table values.
+#' @param var1Base The base value of the first input of the formula.
+#' @param var2Base The base value of the second input of the formula.
+#' @param var1StepSize The step size of the first input of the formula. A negative value flips the order of values.
+#' @param var2StepSize The step size of the second input of the formula. A negative value flips the order of values.
+#' @param var1NumSteps The number of steps above and below the first input base.
+#' @param var2NumSteps The number of steps above and below the second input base.
+#' @param nDigits The number of digits the values will round to.
+#' @param valueFormat What format the values should be. Options are "number" and "percent".
+#' @param returnTable Whether a DT table should be returned or a matrix.
+#'
+#'
+#' @return A DT sensitivity table, or a matrix depending on returnTable.
+#' @export
+#'
+#' @examples
+#' #Generate a sensitivity table for monthly mortgage payments of a 30 year mortgage
+#' #based on the principle and interest rate.
+#'
+#' pmt <- function(prinical, rate) {
+#'   prinical * (rate/12)/(1-(1+rate/12)^-360)
+#' }
+#'
+#' GenerateSensitivityTable(pmt, 340000, .06, 10000, .0025)
+#' GenerateSensitivityTable(pmt, 340000, .06, 10000, .0025, 5, 5, 0, 'number', FALSE)
+#'
+GenerateSensitivityTable <- function(tableFormula,
                                      var1Base = 0,
                                      var2Base = 0,
                                      var1StepSize = 1,
@@ -94,11 +121,11 @@ GenerateSensitivityTable <- function(table_formula,
                                      var1NumSteps = 3,
                                      var2NumSteps = 3,
                                      nDigits = 0,
-                                     value_format = 'number',
+                                     valueFormat = 'number',
                                      returnTable = TRUE) {
 
   # Validate Inputs
-  GenerateSensitivityValidateInputs(table_formula,
+  GenerateSensitivityValidateInputs(tableFormula,
                                     var1Base,
                                     var2Base,
                                     var1NumSteps,
@@ -106,7 +133,7 @@ GenerateSensitivityTable <- function(table_formula,
                                     var1StepSize,
                                     var2StepSize,
                                     nDigits,
-                                    value_format,
+                                    valueFormat,
                                     returnTable)
 
   # Generate matrix for values
@@ -130,11 +157,11 @@ GenerateSensitivityTable <- function(table_formula,
   # Fill values
   for(i in 1:nrow(my_matrix)) {
     for (j in 1:ncol(my_matrix)) {
-      if(value_format == 'percent') {
-        my_matrix[i,j] <- scales::percent(table_formula(as.numeric(rownames(my_matrix)[i]),
+      if(valueFormat == 'percent') {
+        my_matrix[i,j] <- scales::percent(tableFormula(as.numeric(rownames(my_matrix)[i]),
                                                         as.numeric(colnames(my_matrix)[j])), accuracy = desired_accuracy)
       } else {
-        my_matrix[i,j] <- round(table_formula(as.numeric(rownames(my_matrix)[i]),
+        my_matrix[i,j] <- round(tableFormula(as.numeric(rownames(my_matrix)[i]),
                                               as.numeric(colnames(my_matrix)[j])), nDigits)
 
       }
@@ -145,7 +172,7 @@ GenerateSensitivityTable <- function(table_formula,
 
   # Reurn desired output
   if(returnTable) {
-    DT:::datatable(my_matrix)
+    DT::datatable(my_matrix)
   } else {
     my_matrix
   }
